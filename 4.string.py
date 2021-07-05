5f2f80c8d201af5bbbc12925a714f222ede1e412
rayhan <md.rayhan3451@gmail.com>
2021-07-05 12:05:58 +0600

    string added

---
+++ 4.string.py
@@ -0,0 +1,12 @@
+# String is just plain text
+print('Hello World')
+
+print("Hi. How are you")
+
+print("Hi\nHow are you")
+
+name = "rayhan"
+print(name)
+
+print('Hi \How are you')
+print('hi \'How are you')
